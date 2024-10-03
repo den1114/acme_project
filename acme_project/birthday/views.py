7cf3cace73cf3cbcf02a18123078690ef6a2d753
unknown <you@example.com>
2024-10-03 07:55:59 +0300

    qq

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from .forms import BirthdayForm
 # Импортируем из utils.py функцию для подсчёта дней.
 from .utils import calculate_birthday_countdown
-
+from .models import Birthday
 
 def birthday(request):
     form = BirthdayForm(request.POST or None)
@@ -19,4 +19,12 @@ def birthday(request):
         )
         # Обновляем словарь контекста: добавляем в него новый элемент.
         context.update({'birthday_countdown': birthday_countdown})
-    return render(request, 'birthday/birthday.html', context) 
+    return render(request, 'birthday/birthday.html', context)
+
+
+def birthday_list(request):
+    # Получаем все объекты модели Birthday из БД.
+    birthdays = Birthday.objects.all()
+    # Передаём их в контекст шаблона.
+    context = {'birthdays': birthdays}
+    return render(request, 'birthday/birthday_list.html', context)
